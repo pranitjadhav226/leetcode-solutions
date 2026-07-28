8a2a32bf239cd85f50faf1de8839de0f5375f979
pranitjadhav226 <pranitjadhav226@gmail.com>
2026-07-28 08:39:36 -0700

    Create 2149_Rearrange_Array_Elements_by_Sign.py

---
+++ Arrays/2149_Rearrange_Array_Elements_by_Sign.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n 
+        posIndex , negIndex = 0,1
+        for i in range(0 , n) :
+            if nums[i] >= 0 :
+                result[posIndex] = nums[i]
+                posIndex += 2
+            else :
+                result[negIndex] = nums[i]
+                negIndex += 2
+        return result            
