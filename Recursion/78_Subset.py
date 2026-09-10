9bd2d14ea5944112195680fd1ae096909fc3cf65
pranitjadhav226 <pranitjadhav226@gmail.com>
2026-09-10 06:38:21 -0700

    Create 78_Subset.py

---
+++ Recursion/78_Subset.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def func(index, subset):
+            if index >= len(nums):
+                result.append(subset.copy())
+                return
+
+            subset.append(nums[index])
+            func(index + 1, subset)
+
+            subset.pop()
+            func(index + 1, subset)
+
+        func(0, [])
+        return result
